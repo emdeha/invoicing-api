0356b6a73ba932877ec5eadecac6cf41edf7df23
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2022-05-03 08:20:09 +0300

    Adds php-cs-fixer.

---
+++ Makefile
@@ -0,0 +1,2 @@
+lint:
+	tools/php-cs-fixer/vendor/bin/php-cs-fixer fix -vvv bin/main.php
---
+++ bin/main.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Hello world";
+
+if (true === true) {
+    $a = "asfd";
+} else {
+    $b = "bb";
+}
