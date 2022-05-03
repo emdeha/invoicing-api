e2787be26cd515408926f452404a3400bf224408
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2022-05-03 08:27:02 +0300

    Tries to run Slim for handling http requests.

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 lint:
 	tools/php-cs-fixer/vendor/bin/php-cs-fixer fix -vvv bin/main.php
+
+run:
+	php -S localhost:1337 bin/main.php
--- bin/main.php
+++ bin/main.php
@@ -1,9 +1,16 @@
 <?php
 
-echo "Hello world";
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-if (true === true) {
-    $a = "asfd";
-} else {
-    $b = "bb";
-}
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!\n");
+    return $response;
+});
+
+$app->run();
